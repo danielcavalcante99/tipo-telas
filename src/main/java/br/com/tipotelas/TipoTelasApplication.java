a04aac9a86fe3fc98eb24aad4cde85806d0f5786
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-14 13:02:50 -0300

    Projeto Criado

---
+++ src/main/java/br/com/tipotelas/TipoTelasApplication.java
@@ -0,0 +1,13 @@
+package br.com.tipotelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipoTelasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TipoTelasApplication.class, args);
+    }
+
+}
