4ac4abfb6afc13bd9726c4a1541ee8cddf8e5903
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2023-02-14 15:19:45 -0300

    Documentando API

--- src/main/java/br/com/tipotelas/controllers/TipoTelaController.java
+++ src/main/java/br/com/tipotelas/controllers/TipoTelaController.java
@@ -1,8 +1,14 @@
 package br.com.tipotelas.controllers;
 
+import br.com.tipotelas.controllers.exceptions.ApiException;
 import br.com.tipotelas.dtos.TipoTelaDTO;
 import br.com.tipotelas.models.TipoTela;
 import br.com.tipotelas.repositories.TipoTelaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Optional;
 
+@Tag(name="Tipo de Tela")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/tipotela")
@@ -20,6 +27,10 @@ public class TipoTelaController {
     private final TipoTelaRepository repository;
 
     @GetMapping("/v1/{id}")
+    @Operation(summary = "Consultar Tipo de Tela por id")
+    @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Tipo de tela não encotrada",
+    content = @Content(schema = @Schema(defaultValue = "")))
     public ResponseEntity<TipoTelaDTO> buscarPorId(@PathVariable String id) {
 
         Optional<TipoTela> opttipoTela = this.repository.findById(id);
@@ -39,6 +50,10 @@ public class TipoTelaController {
     }
 
     @GetMapping("/v1/tipo")
+    @Operation(summary = "Consultar Tipo de Tela pelo tipo")
+    @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Tipo de tela não encotrada",
+    content = @Content(schema = @Schema(defaultValue = "")))
     public ResponseEntity<TipoTelaDTO> buscarPorTipoDeTela(@RequestParam String tipo) {
 
         Optional<TipoTela> optTipoTela = this.repository.findByTipo(tipo);
@@ -58,7 +73,12 @@ public class TipoTelaController {
     }
 
     @PostMapping("/v1/cadastrar")
-    public ResponseEntity<TipoTelaDTO> buscarPorTipoDeTela(@RequestBody @Valid TipoTelaDTO tpTelaDTO) {
+    @Operation(summary = "Cadastrar Tipo de Tela")
+    @ApiResponse(responseCode = "201", description = "Cadastro realizado com sucesso",
+    content = @Content(schema = @Schema(implementation = TipoTelaDTO.class)))
+    @ApiResponse(responseCode = "400", description = "Requisição inválida",
+    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    public ResponseEntity<TipoTelaDTO> cadastrarTipoDeTela(@RequestBody @Valid TipoTelaDTO tpTelaDTO) {
 
         TipoTelaDTO tipoTelaDTO = TipoTelaDTO.builder()
                 .tipo(tpTelaDTO.getTipo())
@@ -76,6 +96,11 @@ public class TipoTelaController {
 
 
     @DeleteMapping("/v1/excluir/{id}")
+    @Operation(summary = "Exclusão do Tipo de Tela por id")
+    @ApiResponse(responseCode = "204", description = "Exclusão realizada com sucesso",
+    content = @Content(schema = @Schema(defaultValue = "Tipo de tela excluído com sucesso.")))
+    @ApiResponse(responseCode = "404", description = "Tipo de tela não encotrada",
+    content = @Content(schema = @Schema(defaultValue = "Não será possível excluir tipo de tela, pois não existe.")))
     public ResponseEntity<String> excluirPorId(@PathVariable String id) {
 
         Optional<TipoTela> opttipoTela = this.repository.findById(id);
@@ -89,8 +114,15 @@ public class TipoTelaController {
     }
 
 
-    @DeleteMapping("/v1/atualizar")
-    public ResponseEntity<?> excluirPorId(@RequestBody @Valid TipoTelaDTO tpTelaDTO) {
+    @PutMapping("/v1/atualizar")
+    @Operation(summary = "Cadastrar Tipo de Tela")
+    @ApiResponse(responseCode = "201", description = "Cadastro realizado com sucesso",
+    content = @Content(schema = @Schema(implementation = TipoTelaDTO.class)))
+    @ApiResponse(responseCode = "404", description = "Requisição inválida",
+    content = @Content(schema = @Schema(defaultValue = "Não será possível atualizar o tipo de tela, pois não existe.")))
+    @ApiResponse(responseCode = "400", description = "Requisição inválida",
+    content = @Content(schema = @Schema(implementation = ApiException.class)))
+    public ResponseEntity<?> atualizar(@RequestBody @Valid TipoTelaDTO tpTelaDTO) {
 
         Optional<TipoTela> optTipoTela = this.repository.findByTipo(tpTelaDTO.getTipo());
 
